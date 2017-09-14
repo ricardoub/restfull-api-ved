3ac566ba3fa203c76d2078411c300987c9d660bd
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-14 10:01:59 -0300

    video6

--- src/http/routes.js
+++ src/http/routes.js
@@ -4,6 +4,28 @@ const routes = (server) => {
     res.send('Enjoy the silence...!')
     next()
   })
+
+  server.get('categoria', (req, res, next) => {
+    res.send(['1', 'lalala'])
+    next()
+  })
+
+  server.post('categoria', (req, res, next) => {
+    // console.log(req)
+    const { name } = req.params
+    res.send(name)
+    next()
+  })
+
+  // server.put('categoria', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
+
+  // server.delete('categoria', (req, res, next) => {
+  //   res.send()
+  //   next()
+  // })
 }
 
 module.exports = routes
