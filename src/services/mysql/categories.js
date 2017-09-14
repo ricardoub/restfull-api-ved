a74a7d6f3badb1f2e7e01fbcc75ff51dd2fb1def
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-14 12:57:17 -0300

    video12

--- src/services/mysql/categories.js
+++ src/services/mysql/categories.js
@@ -8,9 +8,7 @@ const categories = (deps) => {
           if (error) {
             errorHandler(error, 'Falha ao listar as categorias', reject)
             return false
-            // reject(error)
           }
-          // resolve({ categories: results })
           resolve({
             pagination: {
               page: 1,
@@ -21,9 +19,49 @@ const categories = (deps) => {
         })
       })
     },
-    save: (name) => {},
-    update: (id, name) => {},
-    del: (id) => {}
+    save: (name) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('INSERT INTO categories (name) VALUES (?)', [name], (error, results) => {
+          if (error) {
+            errorHandler(error, `Falha ao salvar a categoria ${name}`, reject)
+            return false
+          }
+          resolve({
+            category: { name, id: results.insertId }
+          })
+        })
+      })
+    },
+    update: (id, name) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('UPDATE categories SET name = ? WHERE id = ?', [name, id], (error, results) => {
+          if (error) {
+            errorHandler(error, `Falha ao atualizar a categoria ${name}`, reject)
+            return false
+          }
+          resolve({
+            category: { name, id: results.insertId }
+          })
+        })
+      })
+    },
+    del: (id) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('DELETE FROM categories WHERE id = ?', [id], (error, results) => {
+          if (error) {
+            errorHandler(error, `Falha ao atualizar a categoria ${id}`, reject)
+            return false
+          }
+          resolve({
+            message: 'Categoria removida com sucesso'
+          })
+        })
+      })
+
+    }
   }
 }
 
